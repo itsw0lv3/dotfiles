f4f8bb652e849247e4a48fea10d73d35b3e6879d
Reece <rerussell@protonmail.com>
2025-07-24 23:43:35 +0200

    Forgot dunst for the joshman

--- hypr_arch.sh
+++ hypr_arch.sh
@@ -3,5 +3,5 @@
 echo Basic Arch Hyprland... I mean basic bruh
 echo ps.... I hope you got that yay installed....
 
-yay -S hyprland hyprland hypridle hyprpaper hyprlock waybar pavucontrol brightnessctl waytrogen wl-clip-persist ttf-dejavu ttf-dejavu-nerd ttf-font-awesome rofi-wayland qt5-wayland qt6-wayland xdg-desktop-portal-hyprland btop
+yay -S hyprland hyprland hypridle hyprpaper hyprlock waybar pavucontrol dunst brightnessctl waytrogen wl-clip-persist ttf-dejavu ttf-dejavu-nerd ttf-font-awesome rofi-wayland qt5-wayland qt6-wayland xdg-desktop-portal-hyprland btop
 
