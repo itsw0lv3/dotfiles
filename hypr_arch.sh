f84997ab289f18041722e828d557febf840354d8
Reece Russell <rerussell@protonmail.com>
2025-07-24 23:33:41 +0200

    Added hypr_arch.sh for the joshman

---
+++ hypr_arch.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo Basic Arch Hyprland... I mean basic bruh
+echo ps.... I hope you got that yay installed....
+
+yay -S hyprland hyprland hypridle hyprpaper hyprlock waybar pavucontrol brightnessctl waytrogen wl-clip-persist ttf-dejavu ttf-dejavu-nerd ttf-font-awesome rofi-wayland qt5-wayland qt6-wayland xdg-desktop-portal-hyprland btop
+
