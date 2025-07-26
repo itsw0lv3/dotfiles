c2c1fa69259762079b4c35a9b7c20054273a097e
Reece Russell <rerurssell@protonmail.com>
2025-07-26 12:50:03 +0200

    Fix hypr_arch.sh install to make easy add packages

--- joshman_confs/hypr_arch.sh
+++ joshman_confs/hypr_arch.sh
@@ -3,5 +3,31 @@
 echo Basic Arch Hyprland... I mean basic bruh
 echo ps.... I hope you got that yay installed....
 
-yay -S hyprland hyprland hypridle hyprpaper hyprlock waybar pavucontrol dunst brightnessctl waytrogen wl-clipboard wl-clip-persist ttf-dejavu ttf-dejavu-nerd ttf-font-awesome rofi-wayland qt5-wayland qt6-wayland xdg-desktop-portal-hyprland btop
+pkgs=(
+hyprland
+hyprland
+hypridle
+hyprpaper
+hyprlock
+waybar
+pavucontrol
+dunst
+brightnessctl
+waytrogen
+wl-clipboard
+wl-clip-persist 
+ttf-dejavu
+ttf-dejavu-nerd
+ttf-font-awesome
+rofi-wayland
+qt5-wayland
+qt6-wayland
+xdg-desktop-portal-hyprland
+btop
+wlogout
+hyprshot
+uwsm
+)
 
+
+yay -S "${pkgs[*]}"
