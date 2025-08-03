05d19694f5bd89649a15f668f0674a45bdb09ccf
Reece Russell <rerussell@protonmail.com>
2025-08-03 11:27:19 +0200

    Added OMZ plugins install script

---
+++ omz-plugins.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+
